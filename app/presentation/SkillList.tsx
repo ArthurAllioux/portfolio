a353d6b68e967db7505887b5f5301198327cc0c8
ArthurAllioux <arthur.allioux@epfedu.fr>
2024-09-21 16:17:18 +0200

    feat: tiny animation on skillList to show hover is allowed

--- app/presentation/SkillList.tsx
+++ app/presentation/SkillList.tsx
@@ -1,38 +1,55 @@
+"use client"
 import Image from "next/image"
+import { cn } from "@/lib/utils"
 
 export default function SkillList({
   skillList,
   title,
+  animate,
 }: {
   skillList: string[]
   title: string
+  animate: boolean
 }) {
   return (
     <div className="mb-4 flex w-full flex-row items-center gap-x-4">
       <h3 className="min-w-24 whitespace-nowrap border-r-2 border-primary pr-4 font-semibold">
         {title}
       </h3>
-      <div className="flex flex-row flex-wrap items-center gap-3">
+      <ul className="flex flex-row flex-wrap items-center gap-3">
         {skillList.map((skill, i) => {
+          const hoverClassText = i === 0 && animate && "opacity-100"
+          const hoverClassImage =
+            i === 0 && animate && "-translate-y-3 lg:-translate-y-4"
           return (
-            <div
-              className="group relative flex flex-col items-center justify-center"
+            <li
+              className={cn(
+                "group relative flex flex-col items-center justify-center",
+              )}
               key={i}
             >
               <Image
-                className="size-8 transition-all group-hover:-translate-y-3 lg:size-12 lg:group-hover:-translate-y-4"
+                className={cn(
+                  "size-8 transition-all group-hover:-translate-y-3 lg:size-12 lg:group-hover:-translate-y-4",
+                  hoverClassImage,
+                )}
                 src={`https://skillicons.dev/icons?i=${skill}`}
                 alt="skills"
                 width={32}
                 height={32}
               />
-              <p className="absolute -bottom-2 text-sm font-medium capitalize italic opacity-0 transition-all group-hover:opacity-100 lg:text-base">
+              <p
+                className={cn(
+                  "absolute -bottom-2 text-sm font-medium capitalize italic opacity-0 transition-all group-hover:opacity-100 lg:text-base",
+                  hoverClassText,
+                )}
+              >
                 {skill}
               </p>
-            </div>
+            </li>
           )
         })}
-      </div>
+      </ul>
     </div>
   )
 }
--- app/presentation/Skills.tsx
+++ app/presentation/Skills.tsx
@@ -1,17 +1,38 @@
+"use client"
 import SkillList from "./SkillList"
+import { useEffect, useState } from "react"
 
 export default function Skills() {
   const frontEndSkills = ["html", "css", "js", "ts", "react", "next"]
   const backEndSkills = ["java", "spring", "ts", "nodejs"]
 
+  const [animateActive, setanimateActive] = useState(false)
+
+  const animating = () => {
+    setanimateActive((prev) => !prev)
+    setTimeout(() => {
+      setanimateActive((prev) => !prev)
+    }, 1500)
+  }
+  useEffect(() => {
+    animating()
+    const intervalId = setInterval(() => {
+      animating()
+    }, 20000)
+    return () => clearInterval(intervalId)
+  }, [])
   return (
     <div className="flex flex-col items-start gap-4">
       <h2 className="w-fit text-xl font-semibold underline decoration-2 underline-offset-4">
         Ma stack
       </h2>
       <div className="flex flex-col items-start">
-        <SkillList skillList={frontEndSkills} title="Front-end" />
-        <SkillList skillList={backEndSkills} title="Back-end" />
+        <SkillList
+          skillList={frontEndSkills}
+          title="Front-end"
+          animate={animateActive}
+        />
+        <SkillList skillList={backEndSkills} title="Back-end" animate={false} />
       </div>
     </div>
   )
